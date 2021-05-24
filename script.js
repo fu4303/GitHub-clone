f49a3a9a143b64c776497f9d7b0e47dbc84fa5f4
pena56 <ogbopinamoses@gmail.com>
2021-05-24 14:56:10 +0100

    adjusted navbar dropdown

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ menuButton.addEventListener('click', function () {
   }
 });
 
-const url = 'https://api.github.com/graphql';
+// const url = 'https://api.github.com/graphql';
 
-async function postData(url = '', data = {}) {
-  const response = await fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify(data),
-  });
-  return response.json();
-}
+// async function postData(url = '', data = {}) {
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     mode: 'cors',
+//     cache: 'no-cache',
+//     credentials: 'same-origin',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     redirect: 'follow',
+//     referrerPolicy: 'no-referrer',
+//     body: JSON.stringify(data),
+//   });
+//   return response.json();
+// }
 
-postData(url, { answer: 42 }).then((data) => {
-  console.log(data);
-});
+// postData(url, { answer: 42 }).then((data) => {
+//   console.log(data);
+// });
